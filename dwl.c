12b44421c84367a67461f81cacf13297ae673c20
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-07-13 20:23:04 -0600

    bump to linux-dmabuf version 5

--- dwl.c
+++ dwl.c
@@ -2468,7 +2468,7 @@ setup(void)
 	if (wlr_renderer_get_texture_formats(drw, WLR_BUFFER_CAP_DMABUF)) {
 		wlr_drm_create(dpy, drw);
 		wlr_scene_set_linux_dmabuf_v1(scene,
-				wlr_linux_dmabuf_v1_create_with_renderer(dpy, 4, drw));
+				wlr_linux_dmabuf_v1_create_with_renderer(dpy, 5, drw));
 	}
 
 	/* Autocreates an allocator for us.
